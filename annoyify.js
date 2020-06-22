d351cfa2cf62a1b8eace0510bec4f1c6252253cf
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2020-06-22 15:14:47 -0400

    music

--- annoyify.js
+++ annoyify.js
@@ -3,4 +3,8 @@ var annoy = function() {
 var all =  document.querySelectorAll(html);
 all.style.color = "green";
 all.style.backgroundColor = "LightGreen"
+var music = document.createElement(audio);
+var audiocode1 = "<audio src=" + "annoying-music.mp3" + " loop autoplay>Error: your browser cannot play obnoxious music</audio>";
+music.innerHTML = audiocode;
+document.body.appendChild(music);
 };
