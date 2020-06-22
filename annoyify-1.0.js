95b077147ee0c33f6533271cf261c224c5903587
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2020-06-22 13:14:05 -0400

    browser extension

--- annoyify-1.0.js
+++ annoyify-1.0.js
@@ -1,4 +1,6 @@
-//main function
-var annoy = function(element) {
-  document.q
-}
+//main function, does everything
+var annoy = function() {
+var all =  document.querySelectorAll(html);
+all.style.color = "green";
+all.style.backgroundColor = "LightGreen"
+};
